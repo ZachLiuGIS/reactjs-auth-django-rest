ef29ee34fe5156f9bdb656bde92b029af928d6a0
ZachLiuGIS <zachliugis@gmail.com>
2016-04-21 23:31:10 -0400

    fixed reset user password.

--- project/urls.py
+++ project/urls.py
@@ -5,6 +5,9 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="project/index.html"), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+
+    # This is used for user reset password
+    url(r'^', include('django.contrib.auth.urls')),
     url(r'^rest-auth/', include('rest_auth.urls')),
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^account/', include('allauth.urls')),
