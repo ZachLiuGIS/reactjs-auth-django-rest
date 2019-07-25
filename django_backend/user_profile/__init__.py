d099dbb1ba3621ee7bf9155ea5a6ffe75a5b07a4
Vitaly <vitalyven@gmail.com>
2019-07-26 00:08:41 +0300

    fix: issue #3
    The extended User class UserProfile does not generate fully when a user account is created
    
    solution proposed by Mehdi6 and alexferrari88

---
+++ django_backend/user_profile/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'user_profile.apps.UserProfileConfig'
--- django_backend/user_profile/apps.py
+++ django_backend/user_profile/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UserProfileConfig(AppConfig):
     name = 'user_profile'
+
+    def ready(self):
+        from .signals import save_profile
